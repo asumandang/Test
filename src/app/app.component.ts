dd0229b2b30f65f15add73c846615d72efdb09d9
AJ Ruth Sumandang <ahsumandang@up.edu.ph>
2017-10-16 22:28:30 +0800

    application legit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
   title = 'applications';
-=======
-  title = 'apppppppplication';
->>>>>>> master
 }
